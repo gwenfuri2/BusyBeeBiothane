d05f48b2a1ff18ed446f1dbe7ab464b3dc0cf8b9
GF <ggfuri2@gmail.com>
2023-07-01 19:14:35 -0700

    changes

--- javascript.js
+++ javascript.js
@@ -6,15 +6,4 @@ function changeTextColor() {
     paragraph.style.color = 'White';
   }
   
-  // Function to display an alert with a cute message
-  function showAlert() {
-    alert("Tell your dog that we said Hi!!");
-  }
-  
-  // Event listener to trigger the changeTextColor() function on page load
-  window.addEventListener('load', changeTextColor);
-  
-  // Event listener to trigger the showAlert() function when the banner text is clicked
-  const bannerText = document.querySelector('.overlay p');
-  bannerText.addEventListener('click', showAlert);
-  
+ 
